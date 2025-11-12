5db9aa96c81772fb37e724941b96967f9029bdc6
Prarthana A C <acprarthana@gmail.com>
2025-11-12 21:59:00 +0530

    Fixed allignmental issues

--- app/Register/page.js
+++ app/Register/page.js
@@ -8,7 +8,7 @@ export default function RegisterPage(){
   const router = useRouter();
   return(
     <div className ="bg-zinc-100 dark:bg-black w-full min-h-screen">
-      <div><Section title="Register Page" title1="register" /></div>
+      <div className="mb-[150px]"><Section title="Register Page" title1="register" /></div>
       <div className="bg-white dark:bg-gray-800 shadow-lg shadow-gray-600 md:h-[700px] h-[1350px] rounded-xl flex md:flex-row flex-col md:mx-[100px] ">
         <div><img className="h-[700px] w-full" src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Fthumb%2F14.jpg&w=1920&q=75"/></div>
         <div className="flex flex-col h-[500px] w-[500px] md:ml-[100px] mt-[76px] gap-5">
@@ -34,7 +34,9 @@ export default function RegisterPage(){
           </div>
         </div>  
       </div>
+  <div className ="mt-[100px]">
       <Footer/>
+  </div>
     </div>
   )
 
