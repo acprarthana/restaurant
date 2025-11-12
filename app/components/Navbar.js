585ddc4f018e07604a747cfa8f521e83c75a9352
Prarthana A C <acprarthana@gmail.com>
2025-11-12 22:07:13 +0530

    Update Navbar links

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -10,11 +10,12 @@ export default function Navbar(){
                     <img className="w-[100px] h-[70px] block dark:hidden" src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Flogo-2.png&w=1920&q=75"/>
                     <img className="w-[100px] h-[70px] hidden dark:block" src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Flogo-light.png&w=750&q=75"></img>
                     <div className ="flex flex-row justify-between items-center gap-10">
+                    <div className="hidden md:block font-semibold text-md dark:text-white">Home</div>>
                     <div onClick={() => router.push("reservation")} className="hidden md:block font-semibold text-md dark:text-white">Reservation</div>
-                    <div onClick={() => router.push("/Menu")} className="hidden md:block font-semibold text-md">Pages</div>
-                    <div onClick={() => router.push("/Register")}  className="hidden md:block font-semibold text-md">Register</div>
+                    <div onClick={() => router.push("/Menu")} className="hidden md:block font-semibold text-md">Menu</div>
                     <div onClick={() => router.push("/Login")} className="hidden md:block font-semibold text-md">Login</div>
-                    <div className="hidden md:blockfont-semibold text-md">Shop</div>
+                    <div onClick={() => router.push("/Register")}  className="hidden md:block font-semibold text-md">Register</div>
+                    <div className="hidden md:blockfont-semibold dark:text-white text-md">Shop</div>
                     <div className="flex items-center"></div>
                      <div className= 'md:ml-[120px] ml-[-150px]  mt-[12] rounded-full overflow-hidden bg-red-500 h-12 w-12 flex flex-row items-center justify-center'>
                         <img src="https://cdn-icons-png.flaticon.com/128/1170/1170678.png" className="w-3/4 h-3/4 object-contain "></img>
